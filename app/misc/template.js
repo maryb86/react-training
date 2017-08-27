45e805779895d41f32924ccc88fe5dd97d519d15
Maciejewski <mary_maciejewski@intuit.com>
2017-08-26 20:39:33 -0600

    3.1

---
+++ app/misc/template.js
@@ -0,0 +1,10 @@
+var name = "tim"
+
+console.log(`Hello, my name is ${name}`);
+console.log(`2 + 2 = ${2+2}`);
+
+function getName() {
+	return 'sue';
+}
+
+console.log(`Hello my name is ${getName()}`);
