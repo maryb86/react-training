7ce09771bf96141ffa0fc01460913c76644b9d9f
Maciejewski <mary_maciejewski@intuit.com>
2017-09-10 19:02:54 -0600

    7.2

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Profile extends React.Component {
+    render() {
+      let hobbies = this.props.hobbies.map(hobby => {
+          return <li>{hobby}</li>
+      })
+      return (
+        <div>
+            <h3>{this.props.name}</h3>
+            <p>{this.props.name} is {this.props.age} and {this.props.bio}</p>
+            <h4>Hobbies</h4>
+            <ul>
+                {hobbies}
+            </ul>
+        </div>
+      )
+    }
+}
+
 class App extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             profiles: [
-                {name: "Sue"},
-                {name: "Bill"}
+                {
+                    name: "Sue",
+                    age: 30,
+                    bio: 'enjoys swimming and biking',
+                    hobbies: ['Swimming', 'biking']
+                },
+                {
+                    name: "Bill",
+                    age: 40,
+                    bio: "enjoys long walks on the beach",
+                    hobbies: ['gardening', 'games']
+                }
             ]
         }
     }
 
     render() {
+        console.log("rendering");
+        let profiles = this.state.profiles.map(profile => {
+            return (
+              <Profile
+                name = {profile.name}
+                age = {profile.age}
+                bio = {profile.bio}
+                hobbies = {profile.hobbies}
+              />
+            )
+        })
         return (
             <div>
-                <p>{this.state.profiles[0].name}</p>
-                <p>{this.state.profiles[1].name}</p>
+              {profiles}
             </div>
         )
     }
